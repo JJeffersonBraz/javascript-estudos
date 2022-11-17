820930dacfdea963c4e008d0e468d170339fc162
Jefferson Braz <jjeffersonbraz@gmail.com>
2022-11-17 20:56:20 -0300

    feat: Adicionado a tabuada de divisão

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,18 +1,27 @@
 function tabuada(){
     let num = document.getElementById('txtnum')
-    let tab = document.getElementById('seltab')
+    let mult = document.getElementById('tabmult')
+    let div = document.getElementById('tabdiv')
 
     if (num.value.length == 0) {
         window.alert('Digite um número')
     } else {
         let n = Number(num.value)
-        tab.innerHTML = ''
+        mult.innerHTML = ''
 
         for (let c=1 ; c<=10 ; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
-            tab.appendChild(item)
+            mult.appendChild(item)
+        }
+
+        div.innerHTML = ''
+        for (let c=1 ; c<=10 ; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} / ${c} = ${n/c}`
+            item.value = `tab${c}`
+            div.appendChild(item)
         }
     }
     
