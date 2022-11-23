2ebd06f412f3382a7576eba97dcea7593eb78112
Jefferson Braz <jjeffersonbraz@gmail.com>
2022-11-23 20:46:16 -0300

    feat: Inclusão das funções isNumber e inList \n\n Verificam se o número informado pelo usuário esta entre o intervalo soliciato e se já foi informado anteriomente

--- analisador-de-numeros/script.js
+++ analisador-de-numeros/script.js
@@ -1,12 +1,35 @@
-function add() {
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let resultado = document.querySelector('div#res')
+let valores = []
 
-    let n = document.getElementById('num')
-    
-    if (n.value == '' || n.value < 1 || n.value > 100) {
+function isNumber(n) {
+    if(n>=1 && n<=100) {
+        return true
+    } else {
         alert('Valor inválido!')
+        return false
     }
+}
 
+function inList(n , l) {
+    if(l.indexOf(n) != -1) {
+        alert('Valor já adicionado a lista!')
+        return false
+    } else {
+        return true
+    }
+}
 
+function add() {
+    if(isNumber(num.value) && inList(num.value,valores)) {
+        valores.push(num.value)
+        let item = document.createElement('option')
+        item.text = `Número ${num.value} adicionado!`
+        lista.appendChild(item)
+    } else {
+        alert('Erro')
+    }
     
 
 }
